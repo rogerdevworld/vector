62bb9f4a5af66fa6e1ffe0141977a055f25b4a48
罗杰 <rogerdevworld@gmail.com>
2025-09-17 21:25:23 +0200

    vec3/vec3_cross.c [OK], norminette [OK]

--- vec3/vec3_cross.c
+++ vec3/vec3_cross.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec3_cross.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero  <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 14:22:49 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/07/23 14:23:20 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../minirt.h"
 
-t_vec3  vec3_cross(t_vec3 v1, t_vec3 v2)
+t_vec3	vec3_cross(t_vec3 v1, t_vec3 v2)
 {
-    t_vec3 result;
+	t_vec3	result;
 
-    result.x = v1.y * v2.z - v1.z * v2.y;
-    result.y = v1.z * v2.x - v1.x * v2.z;
-    result.z = v1.x * v2.y - v1.y * v2.x;
-    return (result);
+	result.x = v1.y * v2.z - v1.z * v2.y;
+	result.y = v1.z * v2.x - v1.x * v2.z;
+	result.z = v1.x * v2.y - v1.y * v2.x;
+	return (result);
 }
