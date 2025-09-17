a613811684ad43f30382f09abf17babe9f1d3d4e
罗杰 <rogerdevworld@gmail.com>
2025-09-17 21:23:57 +0200

    vec2_normalize.c [OK], norminette [OK]

--- vec2/vec2_normalize.c
+++ vec2/vec2_normalize.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec2_normalize.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero  <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 14:22:49 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/07/23 14:23:20 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../minirt.h"
 
-t_vec2  vec2_normalize(t_vec2 v)
+t_vec2	vec2_normalize(t_vec2 v)
 {
-    double length;
+	double	length;
 
-    length = vec2_length(v);
-    if (length < EPSILON)
-        return (vec2_init(0, 0));
-    return (vec2_init(v.x / length, v.y / length));
+	length = vec2_length(v);
+	if (length < EPSILON)
+		return (vec2_init(0, 0));
+	return (vec2_init(v.x / length, v.y / length));
 }
